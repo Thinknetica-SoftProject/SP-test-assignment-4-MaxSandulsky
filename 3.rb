fd0a79b9dcd78d155f92a453682ac18850582fba
MaxSandulsky <78658094+MaxSandulsky@users.noreply.github.com>
2021-03-01 02:37:54 +0300

    Update 3.rb

--- 3.rb
+++ 3.rb
@@ -15,7 +15,7 @@ file = File.open("data/3.txt")
 file_data = file.read
 
 str = ''
-table = Array.new(17) { Array.new(18) { 0 } }
+table = Array.new(17) { Array.new(17) { 0 } }
 
 j, k = 0, 0
 
@@ -38,16 +38,7 @@ end
 
 
 for i in 0..15
-  table[i][16], table[i][17] = table[i][0], table[i][0]
-  for j in 0..15
-    if table[i][j] > table[i][16]
-      table[i][16] = table[i][j]
-    end
-    if table[i][j] < table[i][17]
-      table[i][17] = table[i][j]
-    end
-  end
-  table[i][16] -= table[i][17]
+  table[i][16] = table[i].max - table[i].min
   table[16][0] +=table[i][16]
 end
 puts table[16][0]
